d363fb34972a353228be5b07c3a33d224d9ac41f
Felix Michel <98856469+Felacc@users.noreply.github.com>
2025-05-10 06:40:16 -0700

    feat: setup nvim for writing git commits

---
+++ ftplugin/gitcommit.lua
@@ -0,0 +1,7 @@
+-- Set color column guides for commit messages
+vim.opt_local.colorcolumn = '50,72'
+
+-- Optional: enable spell checking and wrap text
+vim.opt_local.spell = true
+vim.opt_local.textwidth = 72
+vim.opt_local.wrap = true
